87afe54b12a85b818b49422796a8c4d76a0666b0
Your Name <shrestha6941@gmail.com>
2020-07-21 16:59:08 -0500

    Change background image and soome code

---
+++ typescript.js
@@ -0,0 +1,5 @@
+var message = 'Welcome to typescript';
+console.log(message);
+var x = 10;
+var y = 20;
+console.log(x + "" + y);
---
+++ typescript.ts
@@ -0,0 +1,6 @@
+let message:String = 'Welcome to typescript';
+console.log(message);
+
+let x = 10;
+const y=20;
+console.log(x+""+y);
